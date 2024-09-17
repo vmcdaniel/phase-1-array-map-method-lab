2a69e2cc6a04d5e37db074918d4b128bb2209aca
Victoria <vmcdaniel459@gmail.com>
2024-09-17 14:49:31 -0400

    complete

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map((string) => string.split(' ').map(word =>  word[0].toUpperCase() + word.slice(1) ).join(' ')  )
 }
